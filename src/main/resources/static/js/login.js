1c0b704eed0dad716fc3feeb3482f2b2ceba27b7
Oneyoojin <dkffhdp1234@naver.com>
2025-02-13 10:43:30 +0900

    스타일 수정

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   let $signup = $(".links li #signup"); 
-  let $signin = $(".links li #signin");
-  let $reset = $(".links li #reset"); 
+  let $signin = $(".links li #signin"); 
   let $firstInput = $("form").find(".first-input");
   let $hiddenInput = $("form").find(".input__block #repeat__password");
   let $signinBtn = $("form").find(".signin__btn");
+  let $triangle = $(".triangle"); // 삼각형 요소 추가
 
   //----------- sign up ---------------------
   $signup.on("click", function(e) {
@@ -15,6 +15,8 @@ $(document).ready(function() {
     $firstInput.removeClass("first-input__block").addClass("signup-input__block");
     $hiddenInput.stop(true, true).fadeIn(200);  // 애니메이션 효과 추가
     $signinBtn.text("Sign up");
+    $triangle.css("left", "150px"); // 삼각형 위치 변경
+    $(".links li a").css("margin-left", "30mm"); // 버튼을 삼각형 위치에 맞춰 이동
   });
 
   //----------- sign in ---------------------
@@ -23,16 +25,13 @@ $(document).ready(function() {
     $(this).parent().parent().siblings("h1").text("SIGN IN");
     $(this).parent().css("opacity", "1");
     $(this).parent().siblings().css("opacity", ".6");
-    $firstInput.addClass("first-input__block")
-      .removeClass("signup-input__block");
+    $firstInput.addClass("first-input__block").removeClass("signup-input__block");
     $hiddenInput.stop(true, true).fadeOut(200);  // 애니메이션 효과 추가
     $signinBtn.text("Sign in");
+    $triangle.css("left", "50px"); // 삼각형 위치 변경
+    $(".links li a").css("margin-left", "20mm"); // 버튼을 삼각형 위치에 맞춰 이동
   });
 
-  //----------- reset ---------------------
-  $reset.on("click", function(e) {
-    e.preventDefault();
-    $(this).parent().parent().siblings("form")
-    .find(".input__block .input").val("");  // 입력 필드 값 초기화
-  });
+  // OR 구분선 복원
+  $(".separator").html("<p>OR</p>");
 });
